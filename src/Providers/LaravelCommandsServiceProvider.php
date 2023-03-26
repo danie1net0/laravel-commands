96d6f8267069f48f09c4ab15880a26696fa2a345
Daniel Neto <daniel@danielneto.dev.br>
2023-03-26 14:01:40 -0300

    Implement the package provider

---
+++ src/Providers/LaravelCommandsServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ddr\LaravelCommands\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCommandsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+}
